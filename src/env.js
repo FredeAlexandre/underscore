bd627c310b246dd262531e715ff6172b036f5ba6
Alexandre Frede <alexandre.frede.pro@gmail.com>
2024-08-08 19:55:43 +0200

    add some backend endpoints

--- src/env.js
+++ src/env.js
@@ -20,7 +20,14 @@ export const env = createEnv({
       // Since NextAuth.js automatically uses the VERCEL_URL if present.
       (str) => process.env.VERCEL_URL ?? str,
       // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-      process.env.VERCEL ? z.string() : z.string().url()
+      process.env.VERCEL ? z.string() : z.string().url(),
+    ),
+    ELYSIA_URL: z.preprocess(
+      // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
+      // Since NextAuth.js automatically uses the VERCEL_URL if present.
+      (str) => process.env.VERCEL_URL ?? str,
+      // VERCEL_URL doesn't include `https` so it cant be validated as a URL
+      process.env.VERCEL ? z.string() : z.string().url(),
     ),
     DISCORD_CLIENT_ID: z.string(),
     DISCORD_CLIENT_SECRET: z.string(),
@@ -44,6 +51,7 @@ export const env = createEnv({
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    ELYSIA_URL: process.env.ELYSIA_URL,
     DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
     DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
   },
--- src/server/backend/index.ts
+++ src/server/backend/index.ts
@@ -1,10 +1,73 @@
 import { Elysia, t } from "elysia";
+import { treaty } from "@elysiajs/eden";
 import { data } from "./hard_coded_henrik_data";
+import { env } from "~/env";
 
 export const app = new Elysia({ prefix: "/backend" })
-  .get("/premier", data)
+  .get(
+    "/premier/seasons",
+    ({ query, error }) => {
+      if (query.region != "eu")
+        return error(400, `Region ${query.region} has no data implemented yet`);
+      return data;
+    },
+    {
+      query: t.Object({
+        region: t.Union(
+          [
+            t.Literal("eu"),
+            t.Literal("na"),
+            t.Literal("latam"),
+            t.Literal("br"),
+            t.Literal("ap"),
+            t.Literal("kr"),
+          ],
+          { default: "eu" },
+        ),
+      }),
+    },
+  )
+  .get(
+    "/premier/season/:id",
+    ({ query, error, params: { id } }) => {
+      if (query.region != "eu")
+        return error(400, `Region ${query.region} has no data implemented yet`);
+      if (typeof id == "number") {
+        if (id >= data.data.length)
+          return error(400, `Season ${id} doesn't exist`);
+        return data.data[id];
+      } else {
+        if (id == "last") return data.data[data.data.length - 1];
+        const season = data.data.find((s) => s.id === id);
+        if (!season) return error(400, `Season ${id} doesn't exist`);
+        return season;
+      }
+    },
+    {
+      params: t.Object({
+        id: t.Union([t.Number(), t.Literal("last"), t.String()]),
+      }),
+      query: t.Object({
+        region: t.Union(
+          [
+            t.Literal("eu"),
+            t.Literal("na"),
+            t.Literal("latam"),
+            t.Literal("br"),
+            t.Literal("ap"),
+            t.Literal("kr"),
+          ],
+          { default: "eu" },
+        ),
+      }),
+    },
+  )
   .post("/", ({ body }) => body, {
     body: t.Object({
       name: t.String(),
     }),
   });
+
+export type App = typeof app;
+
+export const api = treaty<App>(env.ELYSIA_URL);
