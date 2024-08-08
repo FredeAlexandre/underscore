0af46178f50d029aaded8472817941e9f99da992
Alexandre Frede <alexandre.frede.pro@gmail.com>
2024-08-09 01:32:06 +0200

    add sql schemas

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -8,6 +8,8 @@ import {
   text,
   timestamp,
   varchar,
+  pgEnum,
+  uuid,
 } from "drizzle-orm/pg-core";
 import { type AdapterAccount } from "next-auth/adapters";
 
@@ -18,6 +20,163 @@ import { type AdapterAccount } from "next-auth/adapters";
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
 export const createTable = pgTableCreator((name) => `underscore_${name}`);
+
+export const valorantRegions = createTable("valorant-regions", {
+  id: serial("id").primaryKey(),
+  name: varchar("name"),
+});
+
+export const valorantRegionsRelations = relations(
+  valorantRegions,
+  ({ many }) => ({
+    enrollments: many(premierEnrollments),
+    events: many(premierEvents),
+  }),
+);
+
+export const valorantMaps = createTable("valorant-map", {
+  id: uuid("id").primaryKey(),
+});
+
+export const valorantMapsRelations = relations(valorantMaps, ({ many }) => ({
+  mapsToMatches: many(premierMatches),
+}));
+
+export const premierSeasons = createTable("premier-seasons", {
+  id: serial("id").primaryKey(),
+});
+
+export const premierSeasonsRelations = relations(
+  premierSeasons,
+  ({ many }) => ({
+    enrollments: many(premierEnrollments),
+    events: many(premierEvents),
+    macthes: many(premierMatches),
+  }),
+);
+
+export const premierEnrollments = createTable(
+  "premier-enrollments",
+  {
+    valorantRegionId: integer("valorant_region_id")
+      .notNull()
+      .references(() => valorantRegions.id),
+    premierSeasonId: integer("premier_season_id")
+      .notNull()
+      .references(() => premierSeasons.id),
+    start: timestamp("start", { withTimezone: true }),
+    end: timestamp("end", { withTimezone: true }),
+  },
+  (t) => ({
+    pk: primaryKey({ columns: [t.valorantRegionId, t.premierSeasonId] }),
+  }),
+);
+
+export const premierEnrollmentsRelations = relations(
+  premierEnrollments,
+  ({ one }) => ({
+    region: one(valorantRegions, {
+      fields: [premierEnrollments.valorantRegionId],
+      references: [valorantRegions.id],
+    }),
+    season: one(premierSeasons, {
+      fields: [premierEnrollments.premierSeasonId],
+      references: [premierSeasons.id],
+    }),
+  }),
+);
+
+export const premierMatchType = pgEnum("premier-match-type", [
+  "SCRIM",
+  "LEAGUE",
+  "TOURNAMENT",
+]);
+
+export const premierMatchMapType = pgEnum("premier-match-map-type", [
+  "RANDOM",
+  "PICKBAN",
+]);
+
+export const premierMatches = createTable("premier-matches", {
+  id: serial("id").primaryKey(),
+  premierSeasonId: integer("premier_season_id")
+    .notNull()
+    .references(() => premierSeasons.id),
+  points_required: integer("points_required"),
+  match_type: premierMatchType("match_type"),
+  map_type: premierMatchMapType("map_type"),
+});
+
+export const premierMatchesRelations = relations(
+  premierMatches,
+  ({ many, one }) => ({
+    events: many(premierEvents),
+    maps: many(valorantMaps),
+    season: one(premierSeasons, {
+      fields: [premierMatches.premierSeasonId],
+      references: [premierSeasons.id],
+    }),
+  }),
+);
+
+export const premierMatchesToValorantMaps = createTable(
+  "premier-matches-to-valorants-maps",
+  {
+    valorantMapId: integer("valorant_map_id")
+      .notNull()
+      .references(() => valorantMaps.id),
+    premierMatchId: integer("premier_match_id")
+      .notNull()
+      .references(() => premierMatches.id),
+  },
+  (t) => ({
+    pk: primaryKey({ columns: [t.valorantMapId, t.premierMatchId] }),
+  }),
+);
+
+export const premierMatchesToValorantMapsRelations = relations(
+  premierMatchesToValorantMaps,
+  ({ one }) => ({
+    map: one(valorantMaps, {
+      fields: [premierMatchesToValorantMaps.valorantMapId],
+      references: [valorantMaps.id],
+    }),
+    match: one(premierMatches, {
+      fields: [premierMatchesToValorantMaps.premierMatchId],
+      references: [premierMatches.id],
+    }),
+  }),
+);
+
+export const premierEvents = createTable("premier-events", {
+  id: serial("id").primaryKey(),
+  valorantRegionId: integer("valorant_region_id")
+    .notNull()
+    .references(() => valorantRegions.id),
+  premierSeasonId: integer("premier_season_id")
+    .notNull()
+    .references(() => premierSeasons.id),
+  premierMatchId: integer("premier_matches_id")
+    .notNull()
+    .references(() => premierMatches.id),
+  start: timestamp("start", { withTimezone: true }),
+  end: timestamp("end", { withTimezone: true }),
+});
+
+export const premierEventsRelations = relations(premierEvents, ({ one }) => ({
+  region: one(valorantRegions, {
+    fields: [premierEvents.valorantRegionId],
+    references: [valorantRegions.id],
+  }),
+  season: one(premierSeasons, {
+    fields: [premierEvents.premierSeasonId],
+    references: [premierSeasons.id],
+  }),
+  match: one(premierMatches, {
+    fields: [premierEvents.premierMatchId],
+    references: [premierMatches.id],
+  }),
+}));
 
 export const posts = createTable(
   "post",
@@ -31,13 +190,13 @@ export const posts = createTable(
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
     updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-      () => new Date()
+      () => new Date(),
     ),
   },
   (example) => ({
     createdByIdIdx: index("created_by_idx").on(example.createdById),
     nameIndex: index("name_idx").on(example.name),
-  })
+  }),
 );
 
 export const users = createTable("user", {
@@ -84,7 +243,7 @@ export const accounts = createTable(
       columns: [account.provider, account.providerAccountId],
     }),
     userIdIdx: index("account_user_id_idx").on(account.userId),
-  })
+  }),
 );
 
 export const accountsRelations = relations(accounts, ({ one }) => ({
@@ -107,7 +266,7 @@ export const sessions = createTable(
   },
   (session) => ({
     userIdIdx: index("session_user_id_idx").on(session.userId),
-  })
+  }),
 );
 
 export const sessionsRelations = relations(sessions, ({ one }) => ({
@@ -126,5 +285,5 @@ export const verificationTokens = createTable(
   },
   (vt) => ({
     compoundKey: primaryKey({ columns: [vt.identifier, vt.token] }),
-  })
+  }),
 );
